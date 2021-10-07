373a3d1584c855d26bdf6f3cd7cadd925061b5e2
evan <evan.hermier@gmail.com>
2021-10-07 12:58:29 +0200

    update expectancy table

--- src/components/Expectancy.vue
+++ src/components/Expectancy.vue
@@ -6,20 +6,10 @@
         <div class="text-uppercase custom-data">{{ getRestWeeks }} weeks</div>
         <div class="text-uppercase custom-text">remaining</div>
         <div class="row">
-            <table class="col-6 col-md-4">
+            <table id="square" class="col-6 col-md-4">
                 <tbody>
-                    <tr v-for="n in getFullRow" :key="n">
-                        <td class="fullSquare" v-for="n in 52" :key="n"></td>
-                    </tr>
-                    <tr v-for="n in 1" :key="n">
-                        <td class="fullSquare" v-for="n in getNotFullRow" :key="n"></td>
-                        <td v-for="n in 52 - getNotFullRow" :key="n"></td>
-                    </tr>
-                    <tr v-for="n in getEmptyRow" :key="n">
+                    <tr v-for="n in 80" :key="n">
                         <td v-for="n in 52" :key="n"></td>
-                    </tr>
-                    <tr>
-                        <td v-for="n in 16" :key="n"></td>
                     </tr>
                 </tbody>
             </table>
@@ -45,14 +35,14 @@ export default {
       getRestWeeks () {
           return  4176 - this.getWeeks 
       },
-      getFullRow () {
-          return (this.getWeeks - (this.getWeeks % 52)) / 52
-      },
-      getNotFullRow () {
-          return this.getWeeks % 52
-      },
       getEmptyRow () {
           return Math.floor((this.getRestWeeks - (52 - this.getNotFullRow)) / 52)
+      },
+  },
+  mounted() {
+      const td = document.getElementById("square").getElementsByTagName("td");
+      for(let i = 0; i < this.getWeeks; i++){
+          td[i].classList.add('fullSquare')
       }
   }
 }
